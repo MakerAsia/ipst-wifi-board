d6b4c4712bbaa1394e0fa0ecc20895ff8fc7652c
comdet <listzone@hotmail.com>
2019-10-07 07:45:02 +0700

    update version and remove init block.

--- block/config.js
+++ block/config.js
@@ -7,10 +7,6 @@ let sdcard = require("./menu/config.group.sdcard");
 
 module.exports = {
   //language=HTML format=false
-  initial_blocks: `<xml>
-                            <block type="arduino_init" deletable="false" x="-100" y="-50"></block>
-                            <block type="arduino_loop" deletable="false" x="100" y="-50"></block>
-                      </xml>`,
   blocks: [
     gpio,
     display,
